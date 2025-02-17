f1314f32397f3d96e9daf87250bcd93e3e8a7b37
ds-bhatia <dhruvbhatia.2212@gmail.com>
2025-02-17 14:42:01 +0530

    Added respawning functionality

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,16 +1,12 @@
 extends RigidBody2D
-var speed = 500
+@export var speed = 500
 var velocity = Vector2.ZERO
 var angle
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	angle = [randf_range(-PI/4, PI/4), randf_range(3*PI/4, 5*PI/4)].pick_random()
-	velocity.x = speed * cos(angle)
-	velocity.y = speed * sin(angle)
-	velocity = velocity.normalized()
-
-
+	spawn_ball()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var collision_info = move_and_collide(velocity * delta)
@@ -18,3 +14,14 @@ func _process(delta: float) -> void:
 		speed = 600
 		velocity = velocity.bounce(collision_info.get_normal())
 	position += velocity * speed * delta
+
+func spawn_ball():
+	position = Vector2(578, 320)
+	$RestartTimer.start()
+
+func _on_restart_timer_timeout():
+	position = Vector2(578, 320)
+	angle = [randf_range(-PI/4, PI/4), randf_range(3*PI/4, 5*PI/4)].pick_random()
+	velocity.x = speed * cos(angle)
+	velocity.y = speed * sin(angle)
+	velocity = velocity.normalized()
--- scripts/main.gd
+++ scripts/main.gd
@@ -12,10 +12,11 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+func _on_visible_on_screen_notifier_2d_screen_exited():
 	if $Ball.position.x > 0:
 		p1Score += 1
 		$P1Score.text = str(p1Score)
 	else:
 		p2Score += 1
 		$P2Score.text = str(p2Score)
+	$Ball.spawn_ball()
